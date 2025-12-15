c29747e6668a6d9f169993bcdde92d9bd8d024fc
Mr_Xs <alverojesus5@gmail.com>
2025-12-14 22:45:38 -0400

    logo del yiujo agregado

--- Front/registrodeherencia/src/components/Header.jsx
+++ Front/registrodeherencia/src/components/Header.jsx
@@ -1,11 +1,9 @@
+import logo from "../assets/IUJOLOGO.png";
+
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-4 bg-blue-900 text-white shadow">
-      <img
-        src="/assets/logo-universidad.png"
-        alt="Logo Universidad"
-        className="h-12"
-      />
+      <img src={logo} alt="Logo Universidad" className="h-12" />
       <h1 className="text-2xl font-bold">DApp de Herencias</h1>
     </header>
   );
