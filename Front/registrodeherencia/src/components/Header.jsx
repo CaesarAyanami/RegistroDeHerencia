ab621f2cedbfebaf2101b5ea7e8f25254d1b51c6
CaesarAyanami <cesarcraftero2@gmail.com>
2025-12-14 21:55:35 -0500

    Funciona

--- Front/registrodeherencia/src/components/Header.jsx
+++ Front/registrodeherencia/src/components/Header.jsx
@@ -2,11 +2,11 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between p-4 bg-blue-900 text-white shadow">
       <img
-        src="/assets/logo-universidad.png"
+        src="/assets/logo_universidad.png"
         alt="Logo Universidad"
         className="h-12"
       />
-      <h1 className="text-2xl font-bold">DApp de Herencias</h1>
+      <h1 className="text-2xl font-bold">Panel de Control</h1>
     </header>
   );
 }
